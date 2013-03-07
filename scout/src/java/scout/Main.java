68663bef65250cf370e0c7ba45462155dd4198a1
toum <toum@192.168.1.5>
2013-03-07 15:35:38 +0100

    modif 1

--- scout/src/java/scout/Main.java
+++ scout/src/java/scout/Main.java
@@ -15,7 +15,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("ReTest");
         // TODO code application logic here
     }
 }
