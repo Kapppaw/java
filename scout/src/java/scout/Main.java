80bbdf98da6c51a6d6a1a58bde172b784616892a
toum <toum@192.168.1.5>
2013-03-07 15:25:32 +0100

    first commit

---
+++ scout/src/java/scout/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scout;
+
+/**
+ *
+ * @author toum
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+        // TODO code application logic here
+    }
+}
