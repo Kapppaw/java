c9ede54c0b51322acd7ce1c370cbf955b765acb0
etu21118 <etu21118@IE-011-W3000A48.henallux.be>
2013-03-08 13:52:15 +0100

    test conflict

--- scout/src/java/scout/Main.java
+++ scout/src/java/scout/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Je fais une modif en meme temps que toi");
         //modif depuis moi
         System.out.println("Test");
         System.out.println("je suis thibeal541ut");
