5e4d47acb4fe601b630cbb15dcdca0da7aa761a6
etu16046 <etu16046@IE-011-W3000A49.henallux.be>
2013-03-08 13:44:38 +0100

    modif a la con

--- scout/src/java/scout/Main.java
+++ scout/src/java/scout/Main.java
@@ -14,7 +14,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
         System.out.println("Test");
+        System.out.println("je suis thibealut");
         //edit from web
         // TODO code application logic here
     }
