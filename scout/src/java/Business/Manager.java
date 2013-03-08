471208bfa1ff96fb954d22a4ed9e0983a2a500cd
etu16046 <etu16046@IE-011-W3000A49.henallux.be>
2013-03-08 13:55:45 +0100

    new package

---
+++ scout/src/java/Business/Manager.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author etu16046
+ */
+public class Manager {
+    
+    public Manager () {
+        
+    }
+}
