8cdd186a32af5513e255d993c26a02e2ec19fe9f
etu21118 <etu21118@IE-011-W3000A48.henallux.be>
2013-03-08 13:50:55 +0100

    modif2

--- scout/src/java/scout/Main.java
+++ scout/src/java/scout/Main.java
@@ -15,7 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        
+        //modif depuis moi
         System.out.println("Test");
         System.out.println("je suis thibeal541ut");
         //edit from web
